32dc7f3168386d748c6a6d8e278d39ad1bad0b87
rafael.castro <castro.f.rafael.e@gmail.com>
2017-12-06 15:28:28 +0000

    setup

---
+++ src/main/kotlin/io/vertx/starter/MainVerticle.kt
@@ -0,0 +1,13 @@
+package io.vertx.starter
+
+import io.vertx.core.AbstractVerticle
+
+class MainVerticle : AbstractVerticle() {
+
+  override fun start() {
+    vertx.createHttpServer()
+      .requestHandler { req -> req.response().end("Hello Vert.x!") }
+      .listen(8080)
+  }
+
+}
